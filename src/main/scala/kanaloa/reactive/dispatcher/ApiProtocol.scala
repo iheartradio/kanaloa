da573f0e0fdb3970ae3a612acc1896d7ebe4e817
Kailuo Wang <kailuo.wang@gmail.com>
2015-12-03 14:18:11 -0500

    added SerialVersionUID to apiProtocols (not in normal usages, these messages should remain local)

--- src/main/scala/kanaloa/reactive/dispatcher/ApiProtocol.scala
+++ src/main/scala/kanaloa/reactive/dispatcher/ApiProtocol.scala
@@ -9,17 +9,26 @@ object ApiProtocol {
   sealed trait Response
   sealed trait WorkException extends Response
 
+  @SerialVersionUID(1L)
   case class QueryStatus(replyTo: Option[ActorRef] = None)(implicit sender: ActorRef) extends Request {
     def reply(msg: Any)(implicit replier: ActorRef): Unit = {
       replyTo.getOrElse(sender).!(msg)(replier)
     }
   }
 
+  @SerialVersionUID(1L)
   case class ShutdownGracefully(reportBackTo: Option[ActorRef] = None, timeout: FiniteDuration = 3.minutes) extends Request
+
+  @SerialVersionUID(1L)
   case object ShutdownSuccessfully extends Response
 
+  @SerialVersionUID(1L)
   case class WorkRejected(reason: String) extends WorkException
+
+  @SerialVersionUID(1L)
   case class WorkFailed(reason: String) extends WorkException
+
+  @SerialVersionUID(1L)
   case class WorkTimedOut(reason: String) extends WorkException
 
 }
