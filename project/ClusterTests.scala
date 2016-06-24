437403f16f61088e6aaba26dbe9d1b85df958ce7
Kailuo Wang <kailuo.wang@gmail.com>
2016-06-24 09:08:09 -0400

    disable congregation of multi-jvm test into test

--- project/ClusterTests.scala
+++ project/ClusterTests.scala
@@ -11,19 +11,20 @@ object ClusterTests {
     ),
     compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test),
     // disable parallel tests
-    parallelExecution in Test := false,
+    parallelExecution in Test := false
     // make sure that MultiJvm tests are executed by the default test target,
     // and combine the results from ordinary test and multi-jvm tests
-    executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
-      case (testResults, multiNodeResults)  =>
-        val overall =
-          if (testResults.overall.id < multiNodeResults.overall.id)
-            multiNodeResults.overall
-          else
-            testResults.overall
-        Tests.Output(overall,
-          testResults.events ++ multiNodeResults.events,
-          testResults.summaries ++ multiNodeResults.summaries)
-    }
+//  temporarily disabled since scoverage doesn't support multi-jvm tests
+    //  executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
+//      case (testResults, multiNodeResults)  =>
+//        val overall =
+//          if (testResults.overall.id < multiNodeResults.overall.id)
+//            multiNodeResults.overall
+//          else
+//            testResults.overall
+//        Tests.Output(overall,
+//          testResults.events ++ multiNodeResults.events,
+//          testResults.summaries ++ multiNodeResults.summaries)
+//    }
   )
 }
